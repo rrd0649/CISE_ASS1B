dac5d4286f4f007fa8eb2a76b7db1bbb52da2170
JS <preketchup@gmail.com>
2021-09-21 01:08:17 +1200

    DB connected

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,25 @@
 const express = require("express");
-const articles = require("./dummydata/articles");
+const mongoose = require("mongoose");
+const cors = require("cors");
+//const articles = require("./dummydata/articles");
 const dotenv = require("dotenv");
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 const server = express();
+server.use(cors());
+server.use(express.json());
 
+const uri = process.env.MONGO_URI; // mongoDB Connect 정보
+mongoose.connect(uri, {
+  useNewUrlParser: true
+});
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection succes");
+});
+
+/*
 server.get('/', (req,res) => {
     res.send("API is running")
 })
@@ -19,5 +33,6 @@ server.get('/api/articles/:id', (req,res) => {
     res.send(article);
     console.log(req.params);
 });
+*/
 
 server.listen(PORT, console.log(`server is working and listening on PORT ${PORT}`));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 
 const App = () =>
   <div className="App">
     <h1>Welcome SEPER</h1>
   </div>
-  
+
 export default App;
