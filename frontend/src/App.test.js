a6c1c8445c3a09c50aba77fd051383f6202ff201
JS <preketchup@gmail.com>
2021-10-12 12:43:25 +1300

    test passed

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,7 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
-import CreateArticle from './components/createarticle';
  
 describe('CreateArticle', () => {
   test('renders App component', () => {
